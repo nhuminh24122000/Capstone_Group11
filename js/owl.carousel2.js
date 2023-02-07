89f931d5f3a689737e228ffdeba48c882176f713
HungTran98 <hungtranitvn@gmail.com>
2023-02-07 18:26:29 +0700

    push code first

--- js/owl.carousel2.js
+++ js/owl.carousel2.js
@@ -2963,8 +2963,8 @@
 	Navigation.Defaults = {
 		nav: false,
 		navText: [
-			'<span aria-label="' + 'Previous' + '"> <span class="fa fa-angle-left"></span> </span>',
-			'<span aria-label="' + 'Next' + '"> <span class="fa fa-angle-right"></span> </span>'
+			'<span aria-label="' + 'Previous' + '">&#x2039;</span>',
+			'<span aria-label="' + 'Next' + '">&#x203a;</span>'
 		],
 		navSpeed: false,
 		navElement: 'button type="button" role="presentation"',
